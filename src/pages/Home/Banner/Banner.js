980fb0cfbc7d68218281ec3361af27c9db2a0b55
Musfiqur Rahma <musfiq371@gmail.com>
2021-10-20 23:51:57 +0600

    added responsive

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Row,Button} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Banner.css';
 
 const Banner = () => {
@@ -10,7 +11,8 @@ const Banner = () => {
                     <h1 style={{ fontWeight: '900', color: '#000a54', marginTop: '50px' }}>We Take <span style={{ color: '#e12454' }}>Care</span><br /> Of Your Healthy Life</h1>
                     <p style={{color:'#020c5b',fontWeight:'500'}}>We provide a full range of family care and more than 40 <br /> specialty and subspecialty health care services for patients in our service area. <br />
                         Explore specialized care or select a particular service to learn more.</p>
-                        <Button className='btn-regular'>Read More</Button>
+                    <Link to='/about'>
+                    <Button className='btn-regular'>Read More</Button></Link>
                 </Col>
             </Row>
         </div>
